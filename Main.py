05ef84bed3639345472e57c34af3f24f420a6fcb
Blackout1311 <mikka.widmer@gmx.ch>
2022-07-05 15:20:23 +0200

    added ftp

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
-import requests
+import ftplib
 import json
+from datetime import date
 from urllib.request import urlopen
+import requests
 import yagmail
 from fpdf import FPDF
-from datetime import date
-from ftplib import FTP
 
 API_KEY = "1156afb1ccaf38803781a34203a1daca"
 
@@ -25,14 +25,16 @@ data = requests.get(url).json()
 temp = data['main']['temp']
 humidity = data['main']['humidity']
 
-ftp = FTP('mikkawidmer.bplaced.net')
-ftp.login(user='mikkawidmer', passwd='M!kka2006')
-ftp.cwd('\www')
-
-print(f'In {getcity()} beträgt die Temperatur {temp}°. Die Luftfeuchtigkeit beträgt {humidity}%.')
+zuhause = 'einsiedeln'
+url = f'https://api.openweathermap.org/data/2.5/weather?q={zuhause}&appid={API_KEY}&units=metric'
+data = requests.get(url).json()
+temphome = data['main']['temp']
+humidityhome = data['main']['humidity']
 
 subject = 'Weather'
 content = f'In {getcity()} beträgt die Temperatur {temp}°. Die Luftfeuchtigkeit beträgt {humidity}%.'
+homecontent = f'Zuhause beträgt die Temperatur {temphome}°. Die Luftfeuchtigkeit beträgt {humidityhome}%.'
+print(content)
 
 today = date.today()
 print("Today's date:", today)
@@ -44,32 +46,48 @@ elif (temp > 0):
 elif (temp < 0):
     image = 'snow.png'
 
+if (temphome > 20):
+    imagehome = 'sunny.png'
+elif (temphome > 0):
+    imagehome = 'cloud.png'
+elif (temphome < 0):
+    imagehome = 'snow.png'
 
 
-
-def createPdf(content, today, image):
+def createPdf(content, today, image, homecontent, imagehome):
     pdf = FPDF()
     pdf.add_page()
-    pdf.image(f'{image}', x=75, y=50, w=60)
+    pdf.image(f'{image}', x=75, y=60, w=60)
     pdf.set_font("Arial", size=11)
     pdf.set_text_color(0, 0, 0)
     pdf.text(50, 50, txt=f'{content}')
+    pdf.image(f'{imagehome}', x=75, y=140, w=60)
+    pdf.set_font("Arial", size=11)
+    pdf.set_text_color(0, 0, 0)
+    pdf.text(50, 130, txt=f'{homecontent}')
     pdf.output(f'wheater{today}.pdf')
     return f'wheater{today}.pdf'
 
+createPdf(content, today, image, homecontent, imagehome)
 
-def grabFile():
-    filename = 'index.html'
-    localfile = open(filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
-    ftp.quit()
-    localfile.close()
+server_address = 'mikkawidmer.bplaced.net'
+username = 'mikkawidmer'
+password = 'M!kka2006'
+filename = 'wheater2022-07-05.pdf'
 
-def placeFile():
-    filename = 'index.html'
-    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
-    ftp.quit()
+
+def upload_pdf_ftp(server_address, username, password, filename):
+    # ftp upload of the pdf
+    session = ftplib.FTP(server_address, username, password)
+    file = open(filename, 'rb')  # file to send
+    session.cwd("/www")
+    session.storbinary('STOR ' + filename, file)  # send the file
+    file.close()  # close file and FTP
+    session.quit()
+
+
+upload_pdf_ftp(server_address, username, password, filename)
 
 with yagmail.SMTP(user, app_password) as yag:
-    #yag.send(to, subject, createPdf(content, today, image))
+    # yag.send(to, subject, createPdf(content, today, image))
     print('Sent email successfully')
